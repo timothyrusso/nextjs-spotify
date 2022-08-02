84b762a4ef7eab1ea6d130d0dbaff1a937a317cf
timothyrusso <russotimothy@live.it>
2022-08-02 07:45:07 +0200

    feat: starting sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
 import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
     <div className="">
-      <Head>
-        <title>Spotify 2.0</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-
-      <h1>Spotify 2.0</h1>
-
       <main>
-        {/* sidebar */}
+        <Sidebar />
         {/* center */}
       </main>
 
-      {/* player */}
+      <div>{/* player */}</div>
     </div>
   );
 }
