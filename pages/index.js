af4eb016eb59d471cf588a43cf7640450ae18734
timothyrusso <russotimothy@live.it>
2022-08-02 07:32:51 +0200

    feat: application structure

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <h1>Spotify 2.0</h1>
+
+      <main>
+        {/* sidebar */}
+        {/* center */}
+      </main>
+
+      {/* player */}
+    </div>
+  );
+}
