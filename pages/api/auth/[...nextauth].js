8c93f15c3f317402cbafeb09fbfbb4e043bf14a4
timothyrusso <russotimothy@live.it>
2022-08-11 11:57:11 +0200

    feat: nextauth jvt secret parameter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import SpotifyProvider from "next-auth/providers/spotify"
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import { LOGIN_URL } from '../../../lib/spotify';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +8,8 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      authorization: LOGIN_URL
+      authorization: LOGIN_URL,
     }),
-    // ...add more providers here
   ],
-})
+  secret: process.env.JVT_SECRET,
+});
