32c62b5d6883badbcacf48234e5eb9e58c24818c
timothyrusso <russotimothy@live.it>
2022-08-20 18:19:12 +0200

    feat: styling the login page

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import { getProviders, signIn } from 'next-auth/react';
 
 function login({ providers }) {
   return (
-    <div>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img
         className="w-52 mb-5"
         src="https://links.papareact.com/9xl"
@@ -12,7 +12,9 @@ function login({ providers }) {
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button>Login with {provider.name}</button>
+          <button className="bg-[#18D860] text-white p-5 rounded-full">
+            Login with {provider.name}
+          </button>
         </div>
       ))}
     </div>
