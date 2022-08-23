236914493d31f2b2cff9cf9a0533979e7f9d311e
timothyrusso <russotimothy@live.it>
2022-08-23 07:34:37 +0200

    feat: onClick login button configuration

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,10 @@ function login({ providers }) {
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button className="bg-[#18D860] text-white p-5 rounded-full">
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
             Login with {provider.name}
           </button>
         </div>
