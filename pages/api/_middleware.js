ebb6552222375aeaef6fe06a9e9de1b4fb05768b
timothyrusso <russotimothy@live.it>
2022-08-26 07:11:39 +0200

    feat: next middleware

---
+++ pages/api/_middleware.js
@@ -0,0 +1,22 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  // Token will exist if user is logged in
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  // Allow the requests if the following is true
+  // 1. Its a request for next-auth session & provider fetching
+  // 2. the token exist
+
+  if (pathname.includes('/api/autn') || token) {
+    return NextResponse.next();
+  }
+
+  // Redirect them to login if they don't have token and are requesting a protected route
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
+}
