bde5d4747eefdc44d4fcffdc5557056475b4929f
timothyrusso <russotimothy@live.it>
2022-08-27 15:38:51 +0200

    feat: refactoring the middleware

--- middleware.js
+++ middleware.js
@@ -7,16 +7,19 @@ export async function middleware(req) {
 
   const { pathname } = req.nextUrl;
 
+  if (pathname.startsWith('/_next')) return NextResponse.next();
+
   // Allow the requests if the following is true
   // 1. Its a request for next-auth session & provider fetching
   // 2. the token exist
 
-  if (pathname.includes('/api/autn') || token) {
+  if (pathname.includes('/api/auth') || token) {
     return NextResponse.next();
   }
 
   // Redirect them to login if they don't have token and are requesting a protected route
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect('/login');
+    const loginUrl = new URL('/login', req.url);
+    return NextResponse.redirect(loginUrl);
   }
 }
